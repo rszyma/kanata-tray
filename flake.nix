4c51611d42ea1b684d9f58211216e8d10bce3114
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-03-30 04:41:46 +0530

    feat: added home-manager module & moved package derivation to nix directory
    Added: default kanataPackage & package to home.packages

--- flake.nix
+++ flake.nix
@@ -14,42 +14,9 @@
         runtime-deps = pkgs.lib.optionals (hostPlatform.isLinux) [pkgs.libayatana-appindicator pkgs.gtk3];
         build-deps = [ pkgs.pkg-config ];
       in
-      rec {
-        packages.default = packages.kanata-tray;
-        packages.kanata-tray =
-          pkgs.buildGoModule {
-            name = "kanata-tray";
-            src = pkgs.lib.cleanSource ./.;
-            vendorHash = "sha256-2rR368zzVFhgntVDynXCYNWzM4jalsnDRGaUo81bqIE=";
-            env = { CGO_ENABLED = 1; } // pkgs.lib.attrsets.optionalAttrs (hostPlatform.isDarwin) {
-              GOOS = "darwin";
-              GO111MODULE = "on";
-            };
-
-            flags = [ "-trimpath" ];
-            ldflags = [
-              "-s"
-              "-w"
-              "-X main.buildVersion=nix"
-              "-X main.buildHash=${self.shortRev or self.dirtyShortRev or "unknown"}"
-              "-X main.buildDate=unknown"
-            ];
-            nativeBuildInputs = build-deps;
-            buildInputs = runtime-deps ++ [ pkgs.makeWrapper ];
-            postInstall = ''
-              wrapProgram $out/bin/kanata-tray --set KANATA_TRAY_LOG_DIR /tmp --prefix PATH : $out/bin
-            '';
-            meta = with pkgs.lib; {
-              description = "Tray Icon for Kanata";
-              longDescription = ''
-                A simple wrapper for kanata to control it from tray icon.
-                Works on Windows, Linux and macOS.
-              '';
-              homepage = "https://github.com/rszyma/kanata-tray";
-              license = licenses.gpl3;
-              platforms = platforms.unix;
-            };
-          };
+      {
+        packages.kanata-tray = pkgs.callPackage ./nix/package.nix { inherit build-deps runtime-deps hostPlatform self; };
+        packages.default = self.packages.${system}.kanata-tray;
 
         devShells.default = pkgs.mkShell
           {
@@ -63,5 +30,8 @@
               ];
           };
       }
-    );
+    ) // {
+      homeManagerModules.kanata-tray = import ./nix/hmModule.nix self;
+      homeManagerModules.default = self.homeManagerModules.kanata-tray;
+    };
 }
