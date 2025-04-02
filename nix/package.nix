1a04ede69af8dab4d66a95b237ff2c389dedbf17
Divit Mittal <17973568+DivitMittal@users.noreply.github.com>
2025-04-03 02:29:48 +0530

    feat: add nix home-manager module & add support for darwin systems in nix package derivation  (#51)
    * Moved kanata-tray package derivation to a separate nix/package.nix file.
    * Added support for package derivation to build on darwin systems as well.
    * Added home-manager module (nix/hmModule.nix) for better integration with user environments.

---
+++ nix/package.nix
@@ -0,0 +1,44 @@
+{
+  buildGoModule,
+  lib,
+  hostPlatform,
+  build-deps,
+  runtime-deps,
+  pkgs,
+  self,
+  ...
+}:
+
+buildGoModule {
+  name = "kanata-tray";
+  src = lib.cleanSource ./..;
+  vendorHash = "sha256-2rR368zzVFhgntVDynXCYNWzM4jalsnDRGaUo81bqIE=";
+  env = {
+    CGO_ENABLED = 1;
+    GO111MODULE = "on";
+    GOOS = if hostPlatform.isDarwin then "darwin" else "linux";
+  };
+  flags = [ "-trimpath" ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.buildVersion=nix"
+    "-X main.buildHash=${self.shortRev or self.dirtyShortRev or "unknown"}"
+    "-X main.buildDate=unknown"
+  ];
+  nativeBuildInputs = build-deps;
+  buildInputs = runtime-deps ++ [ pkgs.makeWrapper ];
+  postInstall = ''
+    wrapProgram $out/bin/kanata-tray --set KANATA_TRAY_LOG_DIR /tmp --prefix PATH : $out/bin
+  '';
+  meta = with pkgs.lib; {
+    description = "Tray Icon for Kanata";
+    longDescription = ''
+      A simple wrapper for kanata to control it from tray icon.
+      Works on Windows, Linux and macOS.
+    '';
+    homepage = "https://github.com/rszyma/kanata-tray";
+    license = licenses.gpl3;
+    platforms = platforms.unix;
+  };
+}
