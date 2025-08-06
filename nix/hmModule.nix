e9ef59a5ef633026b9a76d6aa261a33ea8fb9404
rszyma <rszyma.dev@gmail.com>
2025-08-06 04:09:58 +0200

    ci: add nix fmt

--- nix/hmModule.nix
+++ nix/hmModule.nix
@@ -1,61 +1,69 @@
-self: {
+self:
+{
   pkgs,
   lib,
   config,
   hostPlatform,
   ...
-}: let
+}:
+let
   inherit (lib) mkIf;
-  tomlFormat = pkgs.formats.toml {};
+  tomlFormat = pkgs.formats.toml { };
   cfg = config.programs.kanata-tray;
-in {
-  options = let
-    inherit (lib) mkOption mkEnableOption mkPackageOption;
-  in {
-    programs.kanata-tray = {
-      enable = mkEnableOption "kanata-tray";
-      package = mkPackageOption self.packages.${pkgs.system} "kanata-tray" { nullable = true; };
-      settings = mkOption {
-        type = tomlFormat.type;
-        default = {
-          defaults = {
-            kanata_executable = "${pkgs.kanata}/bin/kanata";
-          };
-        };
-        example = lib.literalExpression ''
-          {
-            general = {
-              allow_concurrent_presets = false;
-            };
+in
+{
+  options =
+    let
+      inherit (lib) mkOption mkEnableOption mkPackageOption;
+    in
+    {
+      programs.kanata-tray = {
+        enable = mkEnableOption "kanata-tray";
+        package = mkPackageOption self.packages.${pkgs.system} "kanata-tray" { nullable = true; };
+        settings = mkOption {
+          type = tomlFormat.type;
+          default = {
             defaults = {
-              kanata_executable = "''${pkgs.kanata}/bin/kanata";
-              tcp_port = 5830;
+              kanata_executable = "${self.kanata}/bin/kanata";
             };
           };
-        '';
+          example = lib.literalExpression ''
+            {
+              general = {
+                allow_concurrent_presets = false;
+              };
+              defaults = {
+                kanata_executable = "''${pkgs.kanata}/bin/kanata";
+                tcp_port = 5830;
+              };
+            };
+          '';
 
-        description = ''
-          Configuration written to
-          {file}`$XDG_CONFIG_HOME/kanata-tray/kanata-tray.toml` or for darwin {file}`$HOME/Library/Application Support/kanata-tray/kanata-tray.toml`.
+          description = ''
+            Configuration written to
+            {file}`$XDG_CONFIG_HOME/kanata-tray/kanata-tray.toml` or for darwin {file}`$HOME/Library/Application Support/kanata-tray/kanata-tray.toml`.
 
-          See <https://github.com/rszyma/kanata-tray?tab=readme-ov-file#examples>
-          for the full list of options.
-        '';
+            See <https://github.com/rszyma/kanata-tray?tab=readme-ov-file#examples>
+            for the full list of options.
+          '';
+        };
       };
     };
-  };
 
   config = mkIf (cfg.enable) {
     home.packages = [ cfg.package ];
-    home.file.kanataTray = mkIf (cfg.settings != {}) {
+    home.file.kanataTray = mkIf (cfg.settings != { }) {
       source = tomlFormat.generate "kanata-tray.toml" cfg.settings;
       target =
         (
-          if hostPlatform.isDarwin
-          then "${config.home.homeDirectory}/Library/Application Support"
-          else "${config.xdg.configHome}"
+          if hostPlatform.isDarwin then
+            "${config.home.homeDirectory}/Library/Application Support"
+          else
+            "${config.xdg.configHome}"
         )
         + "/kanata-tray/kanata-tray.toml";
     };
   };
+
+  # TOOD: VM test
 }
