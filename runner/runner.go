rszyma/kanata-tray

commit 5dbd5cd55a6ff2d46bf780433a44bf6ee347fd2d

feat: add extra_env config option (#74)

runner/runner.go
@@ -53,8 +53,16 @@ func NewRunner() *Runner {
 // Calling Run when there's a previous preset running with the the same
 // presetName will block until the previous process finishes.
 // To stop running preset, caller needs to cancel ctx.
-func (r *Runner) Run(ctx context.Context, presetName string, kanataExecutable string,
-	kanataConfig string, tcpPort int, hooks config.Hooks, extraArgs []string, kanataLogFile *os.File,
+func (r *Runner) Run(
+	ctx context.Context,
+	presetName string,
+	kanataExecutable string,
+	kanataConfig string,
+	tcpPort int,
+	hooks config.Hooks,
+	extraArgs []string,
+	extraEnv map[string]string,
+	kanataLogFile *os.File,
 ) error {
 	r.instancesMappingLock.Lock()
 	defer r.instancesMappingLock.Unlock()
@@ -96,7 +104,7 @@ func (r *Runner) Run(ctx context.Context, presetName string, kanataExecutable st
 	}
 
 	instance := r.kanataInstancePool[instanceIndex]
-	err := instance.RunNonblocking(ctx, kanataExecutable, kanataConfig, tcpPort, hooks, extraArgs, kanataLogFile)
+	err := instance.RunNonblocking(ctx, kanataExecutable, kanataConfig, tcpPort, hooks, extraArgs, extraEnv, kanataLogFile)
 	if err != nil {
 		return fmt.Errorf("failed to run kanata: %v", err)
 	}
@@ -162,7 +170,7 @@ func (r *Runner) ServerMessageCh() <-chan ItemAndPresetName[tcp_client.ServerMes
 	return r.serverMessageCh
 }
 
-func cmd(ctx context.Context, stdout io.Writer, stderr io.Writer, name string, args ...string) *exec.Cmd {
+func cmd(ctx context.Context, stdout io.Writer, stderr io.Writer, name string, args []string, extraEnv map[string]string) *exec.Cmd {
 	cmd := exec.CommandContext(ctx, name, args...)
 	cmd.WaitDelay = 3 * time.Second
 	cmd.SysProcAttr = os_specific.ProcessAttr
@@ -172,6 +180,9 @@ func cmd(ctx context.Context, stdout io.Writer, stderr io.Writer, name string, a
 	}
 	if stdout != nil {
 		cmd.Stderr = stderr
+	}
+	for k, v := range extraEnv {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
 	}
 	return cmd
 }

app/app.go
@@ -146,6 +146,7 @@ func (a *SystrayApp) runPreset(presetIndex int, runner *runner_pkg.Runner) {
 		a.presets[presetIndex].Preset.TcpPort,
 		a.presets[presetIndex].Preset.Hooks,
 		a.presets[presetIndex].Preset.ExtraArgs,
+		a.presets[presetIndex].Preset.ExtraEnv,
 		a.presetLogFiles[presetIndex],
 	)
 	if err != nil {

config/config.go
@@ -34,6 +34,7 @@ type Preset struct {
 	LayerIcons         map[string]string
 	Hooks              Hooks
 	ExtraArgs          []string
+	ExtraEnv           map[string]string
 	AutorestartOnCrash bool
 }
 
@@ -73,6 +74,7 @@ type preset struct {
 	LayerIcons         map[string]string `toml:"layer_icons"`
 	Hooks              *hooks            `toml:"hooks"`
 	ExtraArgs          extraArgs         `toml:"extra_args"`
+	ExtraEnv           map[string]string `toml:"extra_env"`
 	AutorestartOnCrash *bool             `toml:"autorestart_on_crash"`
 }
 
@@ -99,6 +101,9 @@ func (p *preset) applyDefaults(defaults *preset) {
 	}
 	if p.ExtraArgs == nil {
 		p.ExtraArgs = defaults.ExtraArgs
+	}
+	if p.ExtraEnv == nil {
+		p.ExtraEnv = defaults.ExtraEnv
 	}
 	if p.AutorestartOnCrash == nil {
 		p.AutorestartOnCrash = defaults.AutorestartOnCrash
@@ -135,6 +140,9 @@ func (p *preset) intoExported() (*Preset, error) {
 			return nil, err
 		}
 		result.ExtraArgs = x
+	}
+	if p.ExtraEnv != nil {
+		result.ExtraEnv = p.ExtraEnv
 	}
 	if p.AutorestartOnCrash != nil {
 		result.AutorestartOnCrash = *p.AutorestartOnCrash

runner/hooks.go
@@ -42,7 +42,8 @@ func runAllBlockingHooks(hooks [][]string, hookType string) error {
 				makeLogWrapWriter(fmt.Sprintf("hook=%d", n), "&1"),
 				makeLogWrapWriter(fmt.Sprintf("hook=%d", n), "&2"),
 				hook[0],
-				hook[1:]...,
+				hook[1:],
+				map[string]string{},
 			)
 			// TODO: capture stdout/stderr?
 			err := cmd.Start()
@@ -91,7 +92,8 @@ func runAllAsyncHooks(ctx context.Context, hooks [][]string, hookType string, an
 			makeLogWrapWriter(fmt.Sprintf("hook=%d", n), "&1"),
 			makeLogWrapWriter(fmt.Sprintf("hook=%d", n), "&2"),
 			hook[0],
-			hook[1:]...,
+			hook[1:],
+			map[string]string{},
 		)
 		// TODO: capture stdout/stderr?
 		err := cmd.Start()

runner/kanata.go
@@ -37,7 +37,7 @@ func NewKanata() *Kanata {
 }
 
 func (r *Kanata) RunNonblocking(ctx context.Context, kanataExecutable string, kanataConfig string,
-	tcpPort int, hooks config.Hooks, extraArgs []string, logFile *os.File,
+	tcpPort int, hooks config.Hooks, extraArgs []string, extraEnv map[string]string, logFile *os.File,
 ) error {
 	if kanataExecutable == "" {
 		var err error
@@ -58,7 +58,7 @@ func (r *Kanata) RunNonblocking(ctx context.Context, kanataExecutable string, ka
 
 	allArgs = append(allArgs, extraArgs...)
 
-	cmd := cmd(ctx, nil, nil, kanataExecutable, allArgs...)
+	cmd := cmd(ctx, nil, nil, kanataExecutable, allArgs, extraEnv)
 
 	go func() {
 		selfCtx, selfCancel := context.WithCancelCause(ctx)
@@ -163,9 +163,13 @@ func (r *Kanata) RunNonblocking(ctx context.Context, kanataExecutable string, ka
 		cmdErr := r.cmd.Wait() // block until kanata exits
 		r.cmd = nil
 
-		log.Infof("Waiting for all post-start-async hooks to exit")
+		if len(hooks.PostStartAsync) > 0 {
+			log.Infof("Waiting for all post-start-async hooks to exit")
+		}
 		<-allPostStartAsyncHooksExitedCh
-		log.Infof("All post-start-async hooks exited")
+		if len(hooks.PostStartAsync) > 0 {
+			log.Infof("All post-start-async hooks exited")
+		}
 
 		err = runAllBlockingHooks(hooks.PostStop, "post-stop")
 		if err != nil {

commit 009e8cbd02d1e0d4f99b58dcfeb1ea8c3e5fea44

fix: inherit env vars for running kanata
it was broken in 5dbd5cd55a

runner/runner.go
@@ -181,6 +181,7 @@ func cmd(ctx context.Context, stdout io.Writer, stderr io.Writer, name string, a
 	if stdout != nil {
 		cmd.Stderr = stderr
 	}
+	cmd.Env = os.Environ()
 	for k, v := range extraEnv {
 		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
 	}